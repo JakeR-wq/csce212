5b26b7c3ddeb7d87b8534f942e8f2429e44d95df
jakeR-wq <jacobr10183@gmail.com>
2024-01-30 17:32:45 -0500

    editing readme

--- for_loops.s
+++ for_loops.s
@@ -11,5 +11,17 @@ main:
 loop:
     bgt $t0, $t1, end #if t0 > t1, jump to end
 
+    move $a0, $t0 #move t0 to a0 (first param for print)
+    li $v0, 1 #system call 1 is for print
+    syscall #print
+
+
+    # still inside the loop
+    # i++
+    addi $t0, $t0, 1    # $t0 = $t0 + 1
+    j loop #jump back to top of loop
+    
+end:
+
     li $v0, 10
     syscall
